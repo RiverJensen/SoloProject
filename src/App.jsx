adc948426bb16b50a35766f39bf6a5387813496e
River Jensen <river@code1088.com>
2023-11-01 16:34:50 -0600

    it broke

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Picture from './component/Picture'
 import FullBox from './component/FullBox'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        test
-        <button>test</button>
-        
-        
-      </div>
 
-      <div>
-        <FullBox></FullBox>
-
-      </div>
-     
-    </>
-  )
+function App({initialData}) {
+  return <FullBox initialUserData = {initialData}/>
 }
 
 export default App
